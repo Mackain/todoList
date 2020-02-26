7e001989088e1c56441883c90ddd2d27cdcfa34d
Markus <markus.j.lindgren@gmail.com>
2020-02-26 20:03:35 +0100

    move ui stuff out of program

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace todoApp
         static void mainMenu()
         {
             todoList todo = new todoList();
+            window ui = new window();
             todo.loadTasksFromDisk();
             string input = "";
             string weekday = DateTime.Now.DayOfWeek.ToString();
@@ -22,24 +23,9 @@ namespace todoApp
             while(true)
             {
                 Console.Clear();
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-                Console.WriteLine(DateTime.Now.ToShortDateString());
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Console.WriteLine("\n$$$$$$$$\\              $$\\           $$\\       $$\\             $$\\     ");
-                Console.WriteLine("\\__$$  __|             $$ |          $$ |      \\__|            $$ |    ");
-                Console.WriteLine("   $$ | $$$$$$\\   $$$$$$$ | $$$$$$\\  $$ |      $$\\  $$$$$$$\\ $$$$$$\\   ");
-                Console.WriteLine("   $$ |$$  __$$\\ $$  __$$ |$$  __$$\\ $$ |      $$ |$$  _____|\\_$$  _|  ");
-                Console.WriteLine("   $$ |$$ /  $$ |$$ /  $$ |$$ /  $$ |$$ |      $$ |\\$$$$$$\\    $$ |    ");
-                Console.WriteLine("   $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |      $$ | \\____$$\\   $$ |$$\\ ");
-                Console.WriteLine("   $$ |\\$$$$$$  |\\$$$$$$$ |\\$$$$$$  |$$$$$$$$\\ $$ |$$$$$$$  |  \\$$$$  |");
-                Console.WriteLine("   \\__| \\______/  \\_______| \\______/ \\________|\\__|\\_______/    \\____/ ");                              
-                Console.WriteLine("\n   =================== [ Tasks for "+ weekday +" ] ==================\n");
+                ui.printHeader();
                 todo.listTasks();
-                Console.WriteLine("\n");
-                 Console.ForegroundColor = ConsoleColor.DarkGray;
-                Console.WriteLine("Enter a task or an index number to complete one:");
-                Console.WriteLine("Use {yyyy-MM-dd} to create tasks for specific dates, and \"!\" to create urgent tasks");
-                Console.ForegroundColor = ConsoleColor.Gray;
+                ui.printFooter();
                 input = Console.ReadLine();
                 int index = 0;
                 bool isNumeric = int.TryParse(input, out index);
